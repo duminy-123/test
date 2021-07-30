a74125d8062d444041300b1cad839964db03cf92
Dumindu Kavinda <duminyk95@gmail.com>
2021-07-30 15:51:48 +0530

    new version of code

--- testcode.py
+++ testcode.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 30 11:27:34 2021
+Created on Fri Jul 30 15:48:56 2021
 
 @author: 123
 """
@@ -8,5 +8,6 @@ Created on Fri Jul 30 11:27:34 2021
 hungry = input("Are You Hungry?")
 if hungry == 'Yes':
     print('Eat Pizza')
+    print('Eat Burger')
 else:
     print('Do Your Exercises')
